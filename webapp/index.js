2b645c7b32dd543c749b88399a2a65a2c58e07b3
izqalan <izqalan@gmail.com>
2020-12-09 18:03:49 +0800

    9. Component Configuration

--- webapp/index.js
+++ webapp/index.js
@@ -1,13 +1,7 @@
 sap.ui.define([
-  "sap/ui/core/mvc/XMLView",
   "sap/ui/core/ComponentContainer"
-], function (XMLView, ComponentContainer) {
+], function (ComponentContainer) {
 	"use strict";
-  // XMLView.create({
-  //   viewName: "sap.ui.demo.walkthrough.view.App"
-  // }).then(function (oView) {
-  //   oView.placeAt("content");
-  // });
   new ComponentContainer({
 		name: "sap.ui.demo.walkthrough",
 		settings : {
